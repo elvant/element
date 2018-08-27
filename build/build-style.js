36380d169c5ea683ae1c564280bb9c3b248d6cc5
Terry Cai <gidcai@gmail.com>
2018-08-27 15:06:33 +0800

    移除

--- build/build-style.js
+++ build/build-style.js
@@ -5,26 +5,6 @@
 // const rename = require('gulp-rename');
 // const autoprefixer = require('gulp-autoprefixer');
 
-// // 编译less
-// gulp.task('css', function () {
-//   gulp.src('../src/styles/index.less')
-//     .pipe(less())
-//     .pipe(autoprefixer({
-//       browsers: ['last 2 versions', 'ie > 8']
-//     }))
-//     .pipe(cleanCSS())
-//     .pipe(rename('iview.css'))
-//     .pipe(gulp.dest('../dist/styles'));
-// });
-
-// // 拷贝字体文件
-// gulp.task('fonts', function () {
-//   gulp.src('../src/styles/common/iconfont/fonts/*.*')
-//     .pipe(gulp.dest('../dist/styles/fonts'));
-// });
-
-// gulp.task('default', ['css', 'fonts']);
-
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
