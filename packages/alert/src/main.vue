8ace1d2a4edd85a8d22e52a1dea1977c833f96fc
Terry Cai <gidcai@gmail.com>
2019-05-19 14:37:16 +0800

    bugfix merge error

--- packages/alert/src/main.vue
+++ packages/alert/src/main.vue
@@ -2,7 +2,7 @@
   <transition name="el-alert-fade">
     <div
       class="el-alert"
-      :class="[typeClass, center ? 'is-center' : '', banner ? 'el-alert--banner' : '']"
+      :class="[typeClass, center ? 'is-center' : '', banner ? 'el-alert--banner' : '', 'is-' + effect]"
       v-show="visible"
       role="alert"
     >
